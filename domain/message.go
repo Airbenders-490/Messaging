9f936905d166600077e47076b6fd0dad7dbf10e2
Zubair Nurie <mznurie@msn.com>
2021-11-28 03:16:37 -0500

    Create domains and define their interfaces
    chat room, rooms for students, message and student domains and their interfaces defined

--- domain/message.go
+++ domain/message.go
@@ -1,16 +1,31 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"github.com/gocql/gocql"
+	"time"
+)
 
-// Chat struct
+// Message struct
 type Message struct {
-	roomID string
+	roomID gocql.UUID
 	SentTimestamp time.Time
 	FromStudentID string
 	MessageBody string
 }
 
-// ChatRepository interface defines the functions all chatRepositories should have
-type ChatRepository interface {
+// MessageRepository interface defines the functions all chatRepositories should have
+type MessageRepository interface {
+	SaveMessage(ctx context.Context, message *Message) error
+	EditMessage(ctx context.Context, message *Message) error
+	GetMessages(ctx context.Context, roomID string, timeStamp time.Time) ([]Message, error)
+	DeleteMessage(ctx context.Context, roomID string, timeStamp time.Time) error
+}
 
+// MessageUseCase defines the functionality messages encapsulate
+type MessageUseCase interface {
+	SendMessage(ctx context.Context, message *Message) error
+	EditMessage(ctx context.Context, userID string, message *Message) error
+	GetMessages(ctx context.Context) ([]Message, error)
+	DeleteMessage(ctx context.Context, roomID string, timeStamp time.Time) error
 }
