ac50e850ef2aaeb2b0850a123b118a8552e1403f
aminebechrifa <58152493+aminebechrifa@users.noreply.github.com>
2021-12-23 21:02:14 -0500

    Repository layer for chatrooms and students rooms

--- domain/chat_room.go
+++ domain/chat_room.go
@@ -1,33 +1,37 @@
 package domain
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 // ChatRoom struct
 type ChatRoom struct {
-	RoomID   string
-	Name     string
-	Admin    Student
+	RoomID string
+	Name string
+	Admin Student
+	Deleted   time.Time
 	Students []Student
 }
 
 // StudentChatRooms struct
 type StudentChatRooms struct {
 	Student Student
-	Rooms   []ChatRoom
+	Rooms []ChatRoom
 }
 
 // RoomRepository interface implements the contract as descirbed aboved each method
 type RoomRepository interface {
-	SaveRoom(ctx context.Context, room *ChatRoom) error
-	GetRoom(ctx context.Context, roomID string) (*ChatRoom, error)
-	GetRoomsFor(ctx context.Context, studentID string) (*StudentChatRooms, error)
+	SaveRoom( room *ChatRoom) error
+	GetRoom(roomID string) (*ChatRoom, error)
+	GetRoomsFor( studentID string) (*StudentChatRooms, error)
 	// EditChatRoomParticipants deals with chat.room and changes students there
-	EditChatRoomParticipants(ctx context.Context, roomID string, student []Student) error
+	EditChatRoomParticipants( roomID string, student []Student) error
 	// AddRoomForParticipants deals with chat.student_rooms and adds the chatroom to each student's list
-	AddRoomForParticipants(ctx context.Context, roomID string, student []Student) error
+	AddRoomForParticipants( roomID string, student []Student) error
 	// RemoveRoomForParticipants deals with chat.student_rooms and removes the chatroom from each student's list
-	RemoveRoomForParticipants(ctx context.Context, roomID string, student []Student) error
-	DeleteRoom(ctx context.Context, roomID string) error
+	RemoveRoomForParticipants( roomID string, student []Student) error
+	DeleteRoom(roomID string) error
 }
 
 // RoomUseCase interface implements the contract as described above each method
@@ -41,3 +45,5 @@ type RoomUseCase interface {
 	// DeleteRoom Ensure the user deleting is the admin
 	DeleteRoom(ctx context.Context, userID string, roomID string) error
 }
+
+
