6cd6a5ee2bb07013a372a88fc301a83ba497cc73
Adam Yafout <adam.yafout@gmail.com>
2022-02-09 23:01:56 -0500

    STUD-234 Smell code reduce from 42 to 4
    chat_delivery_test.go has code smell related to TODO,
    chat_delivery.go has Code Smell related to TODO and Cognitve Complexity,
    middlewares.go Code Smell related to Cognitive Complexity

--- app/mapUrls.go
+++ app/mapUrls.go
@@ -7,12 +7,8 @@ import (
 	"strconv"
 )
 
-<<<<<<< refs/remotes/origin/resolve-techinical-debt/STUD-234
 const pathRoomID = "api/chat/:roomID"
 func mapUrls(mw Middleware, router *gin.Engine, mh *http.MessageHandler, rh *http.RoomHandler) {
-=======
-func mapUrls(mw Middleware, r *gin.Engine, mh *http.MessageHandler, rh *http.RoomHandler) {
->>>>>>> STUD248/app: fix auth for chatroom connection
 
 	r.GET("/chat/:roomID", func(c *gin.Context) {
 		roomID := c.Param("roomID")
@@ -30,14 +26,7 @@ func mapUrls(mw Middleware, r *gin.Engine, mh *http.MessageHandler, rh *http.Roo
 	router.PUT("/rooms/remove/:roomID/:id", rh.RemoveUserFromRoom)
 	router.DELETE("/rooms/:roomID", rh.DeleteRoom)
 
-<<<<<<< refs/remotes/origin/resolve-techinical-debt/STUD-234
-
-	router.GET(pathRoomID, mh.LoadMessages)
-	router.PUT(pathRoomID, mh.EditMessage)
-	router.DELETE(pathRoomID, mh.DeleteMessage)
-=======
 	router.GET("chat/:roomID", mh.LoadMessages)
 	router.PUT("chat/:roomID", mh.EditMessage)
 	router.DELETE("chat/:roomID", mh.DeleteMessage)
->>>>>>> STUD248/app: fix auth for chatroom connection
 }
