1d63f44546be930757b707ef9925e50c003dd369
carlin lee <car12lin12@gmail.com>
2022-03-23 22:32:52 -0400

    try smtp local

--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-	"chat/app"
-	"github.com/joho/godotenv"
+	"fmt"
+	"log"
+	"net/smtp"
 )
 
 func main() {
 
 	//load .env file from given path
-	godotenv.Load(".env")
+	//godotenv.Load(".env")
 
-	app.Start()
+	//app.Start()
+
+	from := "john.doe@example.com"
+	user := "035a001030be3b"
+	password := "5a1e6e53f5f9d8"
+
+	to := []string{
+		"roger.roe@example.com",
+	}
+
+	addr := "smtp.mailtrap.io:2525"
+	host := "smtp.mailtrap.io"
+
+	msg := []byte("From: john.doe@example.com\r\n" +
+		"To: roger.roe@example.com\r\n" +
+		"Subject: Test mail\r\n\r\n" +
+		"Email body\r\n")
+
+	auth := smtp.PlainAuth("", user, password, host)
+
+	err := smtp.SendMail(addr, auth, from, to, msg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Email sent successfully")
 }
